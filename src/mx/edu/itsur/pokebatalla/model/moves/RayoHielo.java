b54a03754b794eab472b4b57f8656fbf2a3ca27c
JoseSgg4 <145613698+JoseSgg4@users.noreply.github.com>
2023-10-21 18:09:03 -0600

    agrego la carpeta de movimientos de los pokemons
    se agrego 9 movimientos

---
+++ src/mx/edu/itsur/pokebatalla/model/moves/RayoHielo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.edu.itsur.pokebatalla.model.moves;
+
+/**
+ *
+ * @author jose
+ */
+public class RayoHielo extends MovimientoEspecial{
+    public RayoHielo() {
+    this.tipo=TiposDeMovimiento.HIELO;
+        this.potencia = 90;
+        this.precision = 100;
+        this.puntosPoder = 10;
+}
+}
