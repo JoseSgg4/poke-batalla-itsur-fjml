4ecb0849682010d9ccd22452e1f45e70f5ab3cdc
JoseSgg4 <145613698+JoseSgg4@users.noreply.github.com>
2023-10-22 18:00:48 -0600

    Actualización de la clase Movimiento.java
    cambias sobre el daño que reciben los pokemons

--- src/mx/edu/itsur/pokebatalla/model/moves/Movimiento.java
+++ src/mx/edu/itsur/pokebatalla/model/moves/Movimiento.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package mx.edu.itsur.pokebatalla.model.moves;
 import mx.edu.itsur.pokebatalla.model.Pokemons.Pokemon;
 /**
  *
- * @author jose
+ * @author Jose Guadalupe Salgado Garcia
  */
 public class Movimiento {
 
@@ -27,45 +23,37 @@ public class Movimiento {
         SINIESTRO,
         TIERRA,
         VENENO,
-        VOLADOR
-        //Segunda Generación
-        //...        
+        VOLADOR      
     }
 
-    //Atributos
+    
     protected TiposDeMovimiento tipo;
     protected int potencia;
     protected int precision;
     protected int puntosPoder;
 
-    //Métodos
+    
     public void utilizar(Pokemon usuario, Pokemon objetivo) {
-        //Calcular el daño
+        
         int nivelAtacante = usuario.getNivel();
         int ataqueAtacante = usuario.getAtaque();
-        int poderMovimiento = this.puntosPoder; //this.getPoder();
+        int poderMovimiento = this.potencia; 
         int defensaObjetivo = objetivo.getDefensa();
         
-        //Calcular el modificador, considerando tipos.
-        double modificador = 1.0; // Modificador base (sin modificaciones)       
-        //if ()... POR HACER        
         
-        int danio = (int) (((
-                ((2 * nivelAtacante / 5 + 2) 
-                        * ataqueAtacante 
-                        * poderMovimiento / defensaObjetivo) 
-                  / 50) + 2) * modificador);
+        double modificador = 1.0;        
         
+        
+        int danio = (int) (((((2 * nivelAtacante / 5 + 2) * ataqueAtacante * poderMovimiento / defensaObjetivo) / 50) + 2) * modificador);
         objetivo.recibirDanio(danio); 
+        
 
-        //Imprimir efecto del movimiento en consola
         System.out.println(
-                         usuario.getClass().getSimpleName() + " aplica " + 
-                         this.getClass().getSimpleName() + " a " +  
-                         objetivo.getClass().getSimpleName() + " y causa danio de " +
+                         usuario.getClass().getSimpleName() + " uso " + 
+                         this.getClass().getSimpleName() + "! a " +  
+                         objetivo.getClass().getSimpleName() + " y le causa danio de " +
                          danio);
-        System.out.println("El objetivo quedo asi: " + objetivo);
+        System.out.println("El pokemon quedo asi: " + objetivo);
         
     }    
 }
-
