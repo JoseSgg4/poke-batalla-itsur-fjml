591eb02de873b86ae67e6e4f7c5457db1dc54291
JoseSgg4 <145613698+JoseSgg4@users.noreply.github.com>
2023-11-12 14:51:29 -0600

    Actualización del  la clase Main.java
    Se agrego un combate

--- src/mx/edu/itsur/pokebatalla/Main.java
+++ src/mx/edu/itsur/pokebatalla/Main.java
@@ -6,6 +6,7 @@ import mx.edu.itsur.pokebatalla.model.Pokemons.Articuno;
 import mx.edu.itsur.pokebatalla.model.Pokemons.Charmander;
 import mx.edu.itsur.pokebatalla.model.Pokemons.Moltres;
 import mx.edu.itsur.pokebatalla.model.Pokemons.Zapdos;
+
 /**
  *
  * @author Jose Guadalupe Salgado Garcia
@@ -16,70 +17,54 @@ public class Main {
         
         System.out.println("Hello PokeBatalla!");
         
-        Pikachu pikachuSalvaje = new Pikachu();
-        Charmander charmanderSalvaje = new Charmander();
-        Bullbasaur bullbasaurSalvaje = new Bullbasaur();
-        Articuno articunoSalvaje = new Articuno();
-        Moltres moltresSalvaje = new Moltres();
-        Zapdos zapdosSalvaje = new Zapdos();
+        Pikachu pika = new Pikachu();
+        Charmander charma = new Charmander();
+        Bullbasaur bullba = new Bullbasaur();
+        Articuno articu = new Articuno();
+        Moltres molt = new Moltres();
+        Zapdos zapd = new Zapdos();
         
-        //Realizar la captura del pikachu salvaje   
-        pikachuSalvaje.setNombre("Pikachu");
-        charmanderSalvaje.setNombre("Charmander");
-        articunoSalvaje.setNombre("Articuno");
-        zapdosSalvaje.setNombre("Zapdos");
-        
-        Pikachu miPikachu = pikachuSalvaje;
-        Articuno miArticuno = articunoSalvaje;
-        Moltres miMoltres = moltresSalvaje;
-        
-        System.out.println("->Pikachu se encuentra con un Charmander y ataca");
-        pikachuSalvaje.atacar(charmanderSalvaje, Pikachu.Movimientos.IMPACTRUENO);
-        
-        System.out.println("->Charmander se molesta y responde ");
-        charmanderSalvaje.atacar(pikachuSalvaje, Charmander.Movimientos.ATAQUE_RAPIDO);
-     
+        System.out.println("Nueva Batalla Pokemon! --------------------------------");
+        System.out.println("");
+        System.out.println("Aperecio un Charmander salvaje!");
+        System.out.println("Adelante, Pikachu!");
+        System.out.println("");
+        pika.atacar(charma, Pikachu.Movimientos.IMPACTRUENO.ordinal());
+        System.out.println("->Charmander utiliza");
+        charma.atacar(pika, Charmander.Movimientos.ATAQUE_RAPIDO.ordinal());
         System.out.println("->Bullbasaur se enoja porque atacaron a su amigo Pikachu y...");
-        bullbasaurSalvaje.atacar(charmanderSalvaje, Bullbasaur.Movimientos.LATIGO);
+        bullba.atacar(charma, Bullbasaur.Movimientos.LATIGO.ordinal()); 
         
         System.out.println("");
         System.out.println("Nueva Batalla Pokemon! --------------------------------");
         System.out.println("");
-        
-        
-        System.out.println("Aperecio un Moltres salvaje!");
+        System.out.println("Aperecio un Charmander salvaje!");
         System.out.println("Adelante, Articuno!");
         System.out.println("");
-        articunoSalvaje.atacar(moltresSalvaje, Articuno.Movimientos.RAYO_HIELO);
+        System.out.println("Articuno uso!");
+        articu.atacar(charma, Articuno.Movimientos.RAYO_HIELO.ordinal());
         System.out.println("");
-        moltresSalvaje.atacar(articunoSalvaje, Moltres.Movimientos.PICOTAZO);
+        System.out.println("Charmander uso!");
+        charma.atacar(articu, Charmander.Movimientos.ATAQUE_RAPIDO.ordinal());
         System.out.println("");
-        articunoSalvaje.atacar(moltresSalvaje, Articuno.Movimientos.ATAQUE_AEREO);
+        System.out.println("Articuno uso!");
+        articu.atacar(charma, Articuno.Movimientos.ATAQUE_AEREO.ordinal());
         System.out.println("");
-        moltresSalvaje.atacar(articunoSalvaje, Moltres.Movimientos.GIRO_FUEGO);
+        System.out.println("Charmander uso!");
+        charma.atacar(articu, Charmander.Movimientos.GRUNIDO.ordinal());
         System.out.println("");
-        articunoSalvaje.atacar(moltresSalvaje, Articuno.Movimientos.SUSTITUTO);
+        System.out.println("Articuno uso!");
+        articu.atacar(charma, Articuno.Movimientos.SUSTITUTO.ordinal());
         System.out.println("");
-        System.out.println("Moltres se va del combate!");
-
+        System.out.println("Charmander uso!");
+        charma.atacar(articu, Charmander.Movimientos.ATAQUE_RAPIDO.ordinal());
         System.out.println("");
-        System.out.println("Nueva Batalla Pokemon! --------------------------------");
+        System.out.println("Articuno uso!");
+        articu.atacar(charma, Articuno.Movimientos.RAYO_HIELO.ordinal());
         System.out.println("");
-        
-        
-        System.out.println("Aperecio un Bullbasaur salvaje!");
-        System.out.println("Adelante, Articuno!");
+        charma.atacar(articu, Charmander.Movimientos.ATAQUE_RAPIDO.ordinal());
+        articu.atacar(charma, Articuno.Movimientos.RAYO_HIELO.ordinal());
         System.out.println("");
-        articunoSalvaje.atacar(bullbasaurSalvaje, Articuno.Movimientos.RAYO_HIELO);
-        System.out.println("");
-        bullbasaurSalvaje.atacar(articunoSalvaje, Bullbasaur.Movimientos.ATAQUE_RAPIDO);
-        System.out.println("");
-        articunoSalvaje.atacar(bullbasaurSalvaje, Articuno.Movimientos.ATAQUE_AEREO);
-        System.out.println("");
-        bullbasaurSalvaje.atacar(articunoSalvaje, Bullbasaur.Movimientos.ATAQUE_RAPIDO);
-        System.out.println("");
-        articunoSalvaje.atacar(bullbasaurSalvaje, Articuno.Movimientos.ATAQUE_AEREO);
-        System.out.println("");
-        System.out.println("Bullbasaur se va del combate!");
+        System.out.println("La batalla a finalizado!");
     }
 }
